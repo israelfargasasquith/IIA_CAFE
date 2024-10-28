eafe48e106e5225c5ab2880ca7a33ede28c0b8b1
JoseRicop <>
2024-10-28 10:03:44 +0100

    Primera Version

--- src/proyecto_iia/Proyecto_IIA.java
+++ src/proyecto_iia/Proyecto_IIA.java
@@ -14,7 +14,7 @@ public class Proyecto_IIA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        IMenu Menu = new Menu();    //MVC
+        IMenu Menu = new Menu();    //Gestión de Opciones
     }
     
 }
