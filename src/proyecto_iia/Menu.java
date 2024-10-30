3f7b8570e794c4938dd9ef13844b6d71fac463bb
JoseRicop <joseferrerestaire@gmail.com>
2024-10-30 11:25:04 +0100

    Segunda Version

--- src/proyecto_iia/Menu.java
+++ src/proyecto_iia/Menu.java
@@ -4,6 +4,10 @@
  */
 package proyecto_iia;
 
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Usuario
@@ -11,22 +15,53 @@ package proyecto_iia;
 public class Menu implements IMenu{
     
     public Menu(){
-        System.out.println("Creando Bebida Caliente...");
         CrearBebida();
-        
-        System.out.println("Bebida Caliente Creada.");
     }
     
     
     
     public Bebida CrearBebida(){
+        int m1 = 0;
         Bebida beb = new Bebida();
-        String tipo = "Caliente";
+        do{
+        Scanner opc1 = new Scanner(System.in);
+        System.out.println("Elige un tipo de bebida: ");
+        System.out.println("1- Bebida Caliente");
+        System.out.println("2- Bebida Fria");
+        System.out.println("3- Salir");
+        System.out.print("    Eleccion ---> ");
+        m1 = opc1.nextInt();
         
-        //Pedir el tipo
         
-        CreadorConcreto c = new CreadorConcreto(tipo);
-        beb = c.Factory_Method();
+            switch(m1){
+                case 1:
+                    String tipo = "Caliente";
+                    CreadorConcreto c = new CreadorConcreto(tipo);
+                    beb = c.Factory_Method();
+                    System.out.println("\nBebida Caliente Creada\n");
+                    break;
+                case 2:
+                    String tipo1 = "Fria";
+                    CreadorConcreto c1 = new CreadorConcreto(tipo1);
+                    beb = c1.Factory_Method();
+                    System.out.println("\nBebida Fria Creada\n");
+                    break;
+                    
+                case 3:
+                    System.out.println("Saliendo...");
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException ex) {
+                        System.out.println("");                    
+                    }
+                    break;                
+
+            }
+            
+        }while(m1>0 && m1<3);
+        
+        
+        
         
         return beb;
     }
