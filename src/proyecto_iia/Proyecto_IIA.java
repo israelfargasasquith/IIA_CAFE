29d52d86207994983f932b4087c6331efe2b1dd0
raeliss <israelfargasasquith@gmail.com>
2024-11-13 11:28:03 +0100

    Fin tarea Splitter y puerto entrada y puerto salida

--- src/proyecto_iia/Proyecto_IIA.java
+++ src/proyecto_iia/Proyecto_IIA.java
@@ -21,6 +21,8 @@ public class Proyecto_IIA {
 
     public static void main(String[] args) throws TransformerConfigurationException, TransformerException {
 
+        
+        //BORRAR LOS GENERATED OUTPUT ANTES DE HACER ALGUNA EJECUCION, ASI VEMOS QUE SE GENERAN NUEVOS
         Slot sEntrada = new Slot();
         Slot sSalida = new Slot();
         ArrayList<Slot> arraySlotEntrada = new ArrayList<>();
@@ -34,122 +36,11 @@ public class Proyecto_IIA {
         puertoEntrada.generarEntrada();
         tareaPrueba.procesar();
         puertoSalida.generarSalida();
-        
-        
-        /*while (!arraySlotSalida.get(0).isEmpty()) {
+
+        /* Para ver la salida de la tarea sin generar fichero, hay que quitar puertoSalida.generarSalida()
+        while (!arraySlotSalida.get(0).isEmpty()) {
             System.out.println(arraySlotSalida.get(0).getMensaje().toString());
 
         }*/
-
     }
 }
-
-/* PRUEBAS PARA CREAR UN FICHERO EXTERNO COMO SALIDA DE DATOS DE LA APLICACION TAMBIEN EJEMPLOS DE COMO EXTRAER DATOS DEL ARBOL DE NODOS 
-DEL XML
-
-Esto para guardarlo en un fichero externo->
-                    //At the end, we save the file XML on disk
-                    //TransformerFactory transformerFactory = TransformerFactory.newInstance();
-                    //Transformer transformer = transformerFactory.newTransformer();
-                    //transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-                    //DOMSource source = new DOMSource(suppXml);
-                    //StreamResult result = new StreamResult(new File("resources/" + supplier.trim() + ".xml"));
-                    //transformer.transform(source, result);
-
-
-                //with StreamResult we generate a new file
-                //StreamResult result = new StreamResult("output.xml");
-                
-                NodeList order_id = transformedDoc.getElementsByTagName("id");
-                NodeList nodos = doc.getElementsByTagName("drink");
-                String id = "";
-
-                if (order_id.getLength() > 0 && order_id.item(0) != null) {
-                    id = order_id.item(0).getTextContent();
-                    System.out.println("Order ID: " + id);
-                } else {
-                    System.out.println("Tag <order_id> not found in the document.");
-                }
-
-                System.out.println("Root Element :" + transformedDoc.getDocumentElement().getNodeName());
-                System.out.println("Order id : " + id);
-                System.out.println("------");
-                for (int temp = 0; temp < nodos.getLength(); temp++) {
-
-                    Node node = nodos.item(temp);
-
-                    if (node.getNodeType() == Node.ELEMENT_NODE) {
-
-                        Element element = (Element) node;
-
-                        String name = element.getElementsByTagName("name").item(0).getTextContent();
-                        String type = element.getElementsByTagName("type").item(0).getTextContent();
-                        String fragmen = element.getElementsByTagName("nFrag").item(0).getTextContent();
-
-                        System.out.println("Current Element :" + node.getNodeName());
-                        System.out.println("First Name : " + name);
-                        System.out.println("Last Name : " + type);
-                        System.out.println("------------");
-
-                    }
-                }
-                /*
-                XPath filter = XPathFactory.newInstance().newXPath();
-                String expressionDrinks = "//drink";
-                String expressionOrderId = "//order_id";
-
-                NodeList order_id = (NodeList) filter.compile(expressionOrderId).evaluate(doc, XPathConstants.NODESET);
-                NodeList nodos = (NodeList) filter.compile(expressionDrinks).evaluate(doc, XPathConstants.NODESET);
-
-                String id = order_id.item(0).getTextContent();
-                System.out.println("Root Element :" + doc.getDocumentElement().getNodeName());
-                System.out.println("Order id : " + id);
-                System.out.println("------");
-                for (int temp = 0; temp < nodos.getLength(); temp++) {
-
-                    Node node = nodos.item(temp);
-
-                    if (node.getNodeType() == Node.ELEMENT_NODE) {
-
-                        Element element = (Element) node;
-
-                        String name = element.getElementsByTagName("name").item(0).getTextContent();
-                        String type = element.getElementsByTagName("type").item(0).getTextContent();
-
-                        System.out.println("Current Element :" + node.getNodeName());
-                        System.out.println("First Name : " + name);
-                        System.out.println("Last Name : " + type);
-                        System.out.println("------------");
-
-                    }
-                }
-
-                //Retrival information without using XPath, just doing a search into the node tree document
-                /* doc.getDocumentElement().normalize();
-
-                NodeList order_id = doc.getElementsByTagName("order_id");
-                String id = order_id.item(0).getTextContent();
-
-                System.out.println("Root Element :" + doc.getDocumentElement().getNodeName());
-                System.out.println("Order id : " + id);
-                System.out.println("------");
-
-                NodeList list = doc.getElementsByTagName("drink");
-                for (int temp = 0; temp < list.getLength(); temp++) {
-
-                    Node node = list.item(temp);
-
-                    if (node.getNodeType() == Node.ELEMENT_NODE) {
-
-                        Element element = (Element) node;
-
-                        String name = element.getElementsByTagName("name").item(0).getTextContent();
-                        String type = element.getElementsByTagName("type").item(0).getTextContent();
-
-                        System.out.println("Current Element :" + node.getNodeName());
-                        System.out.println("First Name : " + name);
-                        System.out.println("Last Name : " + type);
-                        System.out.println("------------");
-
-                    }
-                }*/
